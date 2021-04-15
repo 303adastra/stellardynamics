108fc58b44e505b9596e6b0b7a23018343b9d08a
Sean Marquez <capsulecorplab@gmail.com>
2021-04-15 09:42:26 -0700

    migrate documentation content to docs/ directory
    - add replace /themes/geekdoc w/ docs/themes/geekdoc in .gitmodules
    - update build.sh & serve.sh to run npm & hugo commands in docs/ directory
    - update publish directory to ./docs/public in deploy.yml

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 # first, copy over any extra shortcodes, like the pdf embedded shortcode
 # https://stackoverflow.com/questions/793858/how-to-mkdir-only-if-a-directory-does-not-already-exist
 
-if [ ! -r ./themes/geekdoc/node_modules ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd themes/geekdoc && npm ci'
+if [ ! -r ./docs/themes/geekdoc/node_modules ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs/themes/geekdoc && npm ci'
 fi
-if [ ! -r ./themes/geekdoc/build ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd themes/geekdoc && npx gulp default'
+if [ ! -r ./docs/themes/geekdoc/build ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs/themes/geekdoc && npx gulp default'
 fi
-if [ ! -r ./public ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'hugo --minify -v --destination public'
+if [ ! -r ./docs/public ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs && hugo --minify -v --destination public'
 fi

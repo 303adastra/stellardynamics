90ade735060d9efd4d5d44e8820991f69af90df5
Sean Marquez <capsulecorplab@gmail.com>
2021-04-14 22:55:58 -0700

    update build use remote docker image
    - remove local Dockerfile & Gemfile/Gemfile.lock
    - update build.sh & serve.sh to use capsulecorplab/hugo-asciidoctor-plantuml docker image
    - remove bin/asciidoctor

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-#export PATH="$PWD/bin:$PATH"
-hugo --minify -v --destination public
+# first, copy over any extra shortcodes, like the pdf embedded shortcode
+# https://stackoverflow.com/questions/793858/how-to-mkdir-only-if-a-directory-does-not-already-exist
+
+if [ ! -r ./node_modules ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'npm ci'
+fi
+if [ ! -r ./public ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'hugo --minify -v --destination public'
+fi
--- serve.sh
+++ serve.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#export PATH="$PWD/bin:$PATH"
-hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender --destination public
+docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'hugo serve --bind=0.0.0.0 --buildDrafts --disableFastRender --destination public'

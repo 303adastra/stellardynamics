1442f17c7028a0f0a0debdc83a38c3bbee402850
Sean Marquez <capsulecorplab@gmail.com>
2021-10-02 15:39:01 -0600

    update theme to hugo

--- buildDocs.sh
+++ buildDocs.sh
@@ -3,12 +3,10 @@
 # first, copy over any extra shortcodes, like the pdf embedded shortcode
 # https://stackoverflow.com/questions/793858/how-to-mkdir-only-if-a-directory-does-not-already-exist
 
-if [ ! -r ./docs/themes/geekdoc/node_modules ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs/themes/geekdoc && npm ci'
+if [ ! -r ./node_modules ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'cd docs && npm ci'
 fi
-if [ ! -r ./docs/themes/geekdoc/build ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs/themes/geekdoc && npx gulp default'
-fi
-if [ ! -r ./docs/public ]; then
-    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.82.0-alpine 'cd docs && hugo --minify -v --destination public'
+
+if [ ! -r ./public ]; then
+    docker run --rm --volume $PWD:/src -w "/src" capsulecorplab/hugo-asciidoctor-plantuml:0.76.5-alpine 'cd docs && hugo --minify -v --destination public'
 fi
